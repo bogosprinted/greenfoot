f45041f68453f128ed5a3f92ab776568df222c75
bogosprinted <1218067@student.roc-nijmegen.nl>
2026-06-12 10:00:43 +0200

    heb ze geordend

--- DodoScenario1/MyDodo.java
+++ DodoScenario1/MyDodo.java
@@ -25,12 +25,16 @@ public class MyDodo extends Dodo
             move();     // anders lopen
         }
     }
-
-    public void turn180() {
-        turnRight();
-        turnRight();
+    
+    /**
+     * Returns the number of eggs Dodo has hatched so far.
+     * 
+     * @return int number of eggs hatched by Dodo
+     */
+    public int getNrOfEggsHatched() {
+        return myNrOfEggsHatched;
     }
-
+    
     /**
      * Move one cell forward in the current direction.
      * 
@@ -43,25 +47,6 @@ public class MyDodo extends Dodo
             step();
         } else {
             showError( "I'm stuck!" );
-        }
-    }
-
-    /**
-     * Test if Dodo can move forward, (there are no obstructions
-     *    or end of world in the cell in front of her).
-     * 
-     * <p> Initial: Dodo is somewhere in the world
-     * <p> Final:   Same as initial situation
-     * 
-     * @return boolean true if Dodo can move (no obstructions ahead)
-     *                 false if Dodo can't move
-     *                      (an obstruction or end of world ahead)
-     */
-    public boolean canMove() {
-        if ( fenceAhead() ){
-            return false;
-        } else {
-            return true;
         }
     }
 
@@ -80,183 +65,6 @@ public class MyDodo extends Dodo
         } else {
             showError( "There was no egg in this cell" );
         }
-    }
-
-    public void gotoEgg() 
-    {
-        while(!onEgg())
-        {
-            move(); 
-        }
-    }
-
-    /**
-     * Returns the number of eggs Dodo has hatched so far.
-     * 
-     * @return int number of eggs hatched by Dodo
-     */
-    public int getNrOfEggsHatched() {
-        return myNrOfEggsHatched;
-    }
-
-    /**
-     * Move given number of cells forward in the current direction.
-     * 
-     * <p> Initial:   
-     * <p> Final:  
-     * 
-     * @param   int distance: the number of steps made
-     */
-    public void jump( int distance ) {
-        int nrStepsTaken = 0;               // set counter to 0
-        while ( nrStepsTaken < distance ) { // check if more steps must be taken  
-            move();                         // take a step   
-            nrStepsTaken++;                 // increment the counter
-            System.out.println("moved");
-        }
-    }
-
-    public void climbOverFence()
-    {
-        while(!borderAhead())
-        {
-            
-            if (fenceAhead())
-            { 
-                turnLeft();
-                move();
-                turnRight();
-                move(2);
-                turnRight();
-                move();
-                turnLeft(); 
-            }
-            else
-            {
-                move();
-            }
-            
-        }
-    }
-    
-        public boolean fenceOnRight() {
-        turnRight();          // tijdelijk naar rechts kijken
-        boolean hek = borderAhead();  
-        return hek;
-    }
-
-    public void walkAroundFencedArea() {
-        while (!onEgg()) {
-            move();
-             if (!fenceOnRight()) { 
-            move();
-            } else {
-            move();               
-            }
-        }
-    }
-
-    public void grainAhead(){
-        while(!borderAhead())
-        {
-            if (onGrain())
-            { 
-                pickUpGrain();
-            }
-            else
-            {
-                move();
-            }
-            
-        }
-    }
-    
-    public void stepOneCellBackwards() {
-        turn(180);
-        move(1);
-        turn(180);
-    }
-    
-    public boolean cornerAhead() {
-        turnLeft();
-        boolean eggLeft = onEgg();
-        turnRight(); 
-        
-        turnRight();
-        boolean eggRight = onEgg();
-        turnLeft();
-
-        return eggLeft || eggRight;
-    }
-
-    public void eggTrailToNest() {
-
-        if(onNest()) {
-            System.out.println("arrived");
-        }
-
-        else if (onEgg()) {
-            move(); 
-        }
-
-        else if (cornerAhead()) {
-            turnTowardsEgg();
-            move();
-        }
-
-        else {
-            gotoEgg(); 
-        }
-    }
-
-    public void turnTowardsEgg() {
-        turnLeft();
-        if (onEgg()) {
-            return; // we staan al goed
-        }
-        turnRight(); 
-        turnRight();
-        if (onEgg()) {
-            return;
-        }
-        turnLeft(); 
-    }
-    
-    public void layTrailofEggs( int n ) {
-        int nrStepsTaken = 0;               
-        while ( nrStepsTaken < n ) {   
-            move();   
-            layEgg();
-            nrStepsTaken++;                 
-        }
-    }
-    
-    
-    
-    /**
-     * Walks to edge of the world printing the coordinates at each step
-     * 
-     * <p> Initial: Dodo is on West side of world facing East.
-     * <p> Final:   Dodo is on East side of world facing East.
-     *              Coordinates of each cell printed in the console.
-     */
-
-    public void walkToWorldEdgePrintingCoordinates( ){
-        while( ! borderAhead() ){
-            // print coordinates
-            printCoordinates(getX(), getY());
-            move();
-        }
-    }
-
-    public void goBackToStartOfRowAndFaceBack() {
-        turn180();
-        walkToWorldEdgePrintingCoordinates();
-        turn180();
-    }
-
-    public void printCoordinates(int x, int y) {
-        System.out.println("Co?rdinaten: (" + x + ", " + y + ")");
     }
 
     /**
@@ -280,6 +88,136 @@ public class MyDodo extends Dodo
         }
     }
     
+    public void turn180() {
+        turnRight();
+        turnRight();
+    }
+    
+    /**
+     * Move given number of cells forward in the current direction.
+     * 
+     * <p> Initial:   
+     * <p> Final:  
+     * 
+     * @param   int distance: the number of steps made
+     */
+    public void jump( int distance ) {
+        int nrStepsTaken = 0;               // set counter to 0
+        while ( nrStepsTaken < distance ) { // check if more steps must be taken  
+            move();                         // take a step   
+            nrStepsTaken++;                 // increment the counter
+            System.out.println("moved");
+        }
+    }
+    
+    /**
+     * Walks to edge of the world printing the coordinates at each step
+     * 
+     * <p> Initial: Dodo is on West side of world facing East.
+     * <p> Final:   Dodo is on East side of world facing East.
+     *              Coordinates of each cell printed in the console.
+     */
+
+    public void walkToWorldEdgePrintingCoordinates( ){
+        while( ! borderAhead() ){
+            // print coordinates
+            printCoordinates(getX(), getY());
+            move();
+        }
+    }
+    
+    /**
+     * Test if Dodo can move forward, (there are no obstructions
+     *    or end of world in the cell in front of her).
+     * 
+     * <p> Initial: Dodo is somewhere in the world
+     * <p> Final:   Same as initial situation
+     * 
+     * @return boolean true if Dodo can move (no obstructions ahead)
+     *                 false if Dodo can't move
+     *                      (an obstruction or end of world ahead)
+     */
+    public boolean canMove() {
+        if ( fenceAhead() ){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public void WalkToWorldEdgeClimbOverFence()
+    {
+        while(!borderAhead())
+        {
+            
+            if (fenceAhead())
+            { 
+                turnLeft();
+                move();
+                turnRight();
+                move(2);
+                turnRight();
+                move();
+                turnLeft(); 
+            }
+            else
+            {
+                move();
+            }
+        }
+    }
+    
+    public void grainAhead(){
+        while(!borderAhead())
+        {
+            if (onGrain())
+            { 
+                pickUpGrain();
+            }
+            else
+            {
+                move();
+            }
+            
+        }
+    }
+    
+    public void gotoEgg() 
+    {
+        while(!onEgg())
+        {
+            move(); 
+        }
+    }
+    
+    public void goBackToStartOfRowAndFaceBack() {
+        turn180();
+        walkToWorldEdgePrintingCoordinates();
+        turn180();
+    }
+    
+    public void pickUpGrainsAndPrintCoordinates() {
+        while(!borderAhead())
+        {
+            if (onGrain())
+            { 
+                pickUpGrain();
+                printCoordinates(getX(), getY());
+            }
+            else
+            {
+                move();
+            }
+            
+        }
+    }
+    
+    public void stepOneCellBackwards() {
+        turn(180);
+        move(1);
+        turn(180);
+    }
+    
     public void worldEmptyNestsTopRow() {
         while(!borderAhead())
         {
@@ -294,5 +232,165 @@ public class MyDodo extends Dodo
             }
             
         }
+    }
+    
+    public void walkAroundFencedArea() {
+        while (!onEgg()) {
+            move();
+             if (!fenceOnRight()) { 
+            move();
+            } else {
+            move();               
+            }
+        }
+    }
+    
+    public void eggTrailToNest() {
+        if(onNest()) {
+            System.out.println("arrived");
+        }
+
+        else if (onEgg()) {
+            move(); 
+        }
+
+        else if (cornerAhead()) {
+            turnTowardsEgg();
+            move();
+        }
+
+        else {
+            gotoEgg(); 
+        }
+    }
+    
+    public void worldMazeLevel1() {
+        
+    }
+    
+    public void worldMazeLevel2() {
+        
+    }
+    
+    public void tijdelijkeWaardeEi() {
+        
+    }
+    
+    public void faceEast() {
+        
+    }
+    
+    public void GetDirection() {
+        
+    }
+    
+    public void goToLocation(int x, int y) {
+        setLocation(x, y);
+    }
+    
+    public void validCoordinates(int x, int y) {
+        
+    }
+    
+    public int countEggsInRow(int y) {
+        int count = 0;
+        int width = getWorld().getWidth();
+        for (int x = 0; x < width; x++) {
+            goToLocation(x, y);
+            if (onEgg()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void layTrailofEggs( int n ) {
+        int nrStepsTaken = 0;               
+        while ( nrStepsTaken < n ) {   
+            move();   
+            layEgg();
+            nrStepsTaken++;                 
+        }
+    }
+    
+    public void countAllEggsInWorld() {
+        int total = 0;
+        int height = getWorld().getHeight();
+
+        for (int y = 0; y < height; y++) {
+            total += countEggsInRow(y);
+        }
+
+        System.out.println("Totaal aantal eieren: " + total);
+    }
+    
+    public void findRowWithMostEggs() {
+        int maxEggs = 0;            
+        int rowWithMostEggs = 0;     
+        int height = getWorld().getHeight();
+
+        for (int y = 0; y < height; y++) {
+            int eggsInRow = countEggsInRow(y); 
+            if (eggsInRow > maxEggs) {         
+                maxEggs = eggsInRow;           
+                rowWithMostEggs = y;           
+            }
+        }
+        System.out.println("Rij met meeste eieren: " + rowWithMostEggs);
+        goToLocation(0, 0);  
+    }
+    
+    public void eggMonument() {
+        
+    }
+    
+    public void stevigMonument() {
+        
+    }
+    
+    public void eggPiramide() {
+        
+    }
+    
+    public void gemiddeldeAantalEggsPerRow() {
+        
+    }
+    
+    
+    
+    
+        public boolean fenceOnRight() {
+        turnRight();          // tijdelijk naar rechts kijken
+        boolean hek = borderAhead();  
+        return hek;
+    }
+    
+    public boolean cornerAhead() {
+        turnLeft();
+        boolean eggLeft = onEgg();
+        turnRight(); 
+        
+        turnRight();
+        boolean eggRight = onEgg();
+        turnLeft();
+
+        return eggLeft || eggRight;
+    }
+
+    public void turnTowardsEgg() {
+        turnLeft();
+        if (onEgg()) {
+            return; // we staan al goed
+        }
+        turnRight(); 
+        turnRight();
+        if (onEgg()) {
+            return;
+        }
+        turnLeft(); 
+    }
+    
+    public void printCoordinates(int x, int y) {
+        System.out.println("Co?rdinaten: (" + x + ", " + y + ")");
     }
 }  
